ce4cab4b77364ac02ab5fef599e12ed16bb20802
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2026-01-17 10:45:19 -0800

    layout.tsx

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import NavBarFacts from "../components/NavBarFacts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <NavBarFacts />
+        {children}
+      </body>
+    </html>
+  );
+}
